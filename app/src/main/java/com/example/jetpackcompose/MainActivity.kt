964898420f088000a91cff884fa73f6b92f4c3fc
thinhtrai1 <thinhnd.nta@gmail.com>
2022-02-19 16:24:10 +0700

    Copy

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,13 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.core.os.postDelayed
-import com.example.jetpackcompose.main.InformationView
 import com.example.jetpackcompose.main.PagerView
-import com.google.accompanist.pager.ExperimentalPagerApi
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,23 +27,10 @@ class SplashActivity : ComponentActivity() {
 }
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterialApi
-    @ExperimentalComposeUiApi
-    @ExperimentalUnitApi
-    @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PagerView()
-        }
-    }
-}
-
-class InformationActivity: ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            InformationView()
         }
     }
 }
--- app/src/main/java/com/example/jetpackcompose/main/PagerApp.kt
+++ app/src/main/java/com/example/jetpackcompose/main/PagerApp.kt
@@ -1,284 +1,218 @@
 package com.example.jetpackcompose.main
 
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.util.lerp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.core.os.postDelayed
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
-import com.example.jetpackcompose.InformationActivity
 import com.example.jetpackcompose.R
 import com.google.accompanist.pager.*
+import com.google.accompanist.pager.VerticalPager
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
-@ExperimentalMaterialApi
-@ExperimentalComposeUiApi
-@ExperimentalUnitApi
-@ExperimentalPagerApi
+class PageData(
+    val title: String,
+    val child: List<String>
+)
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalCoilApi::class)
 @Composable
-@Preview
 fun PagerView() {
-    val features = listOf(
-        Feature(
-            "Home",
-            listOf("Home", "Notification", "Information", "Setting")
+    val pages = listOf(
+        PageData(
+            title = "Home",
+            child = listOf("Notification", "Information", "Setting")
         ),
-        Feature(
-            "Communication",
-            listOf("News", "Blog", "Movie", "Photo", "Music")
+        PageData(
+            title = "Communication",
+            child = listOf("News", "Blog", "Movie", "Photo", "Music")
         ),
-        Feature(
-            "Original content",
-            listOf("Community", "Point", "Favourite", "Gatherings", "Happy")
+        PageData(
+            title = "Original content",
+            child = listOf("Community", "Point", "Favourite", "Gatherings")
         ),
     )
-    val coroutineScope = rememberCoroutineScope()
+
+    val random = (0..1000).random()
     val horizontalPagerState = rememberPagerState(1000)
-    val context = LocalContext.current
-
-    fun PagerScope.graphics(page: Int, isVertical: Boolean) = Modifier
-        .graphicsLayer {
-            val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
-            lerp(
-                start = 0.9f,
-                stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
-            ).let { scale ->
-                if (isVertical) {
-                    scaleX = scale
-                    scaleY = scale
-                } else {
-                    scaleX = scale
-                    scaleY = scale
-                }
-            }
-            alpha = lerp(
-                start = 0.5f,
-                stop = 1f,
-                fraction = 1f - pageOffset.coerceIn(0f, 1f)
-            )
-            Log.e("pageOffset", "pageOffset: $pageOffset")
-//            if (pageOffset == 0f) {
-//                scaleX = 1f
-//                scaleY = 1f
-//            }
-        }
-        .fillMaxSize()
-
-    HorizontalPager(
-        count = Int.MAX_VALUE,
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(horizontal = 32.dp),
-        state = horizontalPagerState
-    ) { page ->
-        Box(modifier = graphics(page, false)) {
-            VerticalPager(
+    Scaffold {
+        Box {
+            HorizontalPager(
                 count = Int.MAX_VALUE,
                 modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(vertical = 64.dp),
-                state = PagerState(1000)
-            ) { page2 ->
-                Card(
-                    modifier = graphics(page2, true),
-                    shape = RoundedCornerShape(16.dp),
-                    onClick = {
-                        context.startActivity(Intent(context, InformationActivity::class.java))
-//                    coroutineScope.launch {
-//                        if (page < 9) {
-//                            pagerState.animateScrollToPage(page = page + 1)
-//                        }
-//                        if (page2 < 9) {
-//                            verticalPagerState[page].animateScrollToPage(page2 + 1)
-//                            if (page < 8)
-//                            verticalPagerState[page + 1].animateScrollToPage(page2 + 1)
-//                            if (page < 7) {
-//                                verticalPagerState[page + 2].animateScrollToPage(page2 + 1)
-//                            }
-//                        }
-//                    }
-                    }
-                ) {
-                    Box {
-                        val seed = rangeForRandom.random()
-                        Image(
-                            painter = rememberImagePainter(
-                                data = rememberRandomSampleImageUrl(
-                                    seed = seed,
-                                    width = 600,
-                                    height = 1200
-                                ),
-                            ),
-                            contentScale = ContentScale.Crop,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .background(Color.LightGray),
+                contentPadding = PaddingValues(horizontal = 32.dp),
+                state = horizontalPagerState
+            ) { page ->
+                val p1 = page % pages.size
+                val selected = remember { mutableStateOf(false) }
+                val scale = animateFloatAsState(if (selected.value) 0.95f else 1f)
+                val pagerState = rememberPagerState(1000)
+                VerticalPager(
+                    count = Int.MAX_VALUE,
+                    contentPadding = PaddingValues(vertical = 40.dp),
+                    itemSpacing = 20.dp,
+                    modifier = Modifier.graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+                        lerp(
+                            start = 0.9f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).let { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
                         )
-//                        ProfilePicture(
-//                            seed,
-//                            Modifier
-//                                .align(Alignment.BottomCenter)
-//                                .padding(16.dp)
-//                        )
-                        Card(
-                            shape = RoundedCornerShape(16.dp),
-                            backgroundColor = Color.White,
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                                .width(96.dp)
-                                .height(96.dp)
-                        ) {
-                            Text(
-                                text = features[page % features.size].list[page2 % features[page % features.size].list.size],
-                                fontWeight = FontWeight.ExtraBold,
-                                fontSize = TextUnit(16f, TextUnitType.Sp),
-                                modifier = Modifier
-                                    .wrapContentSize(),
-                            )
-                        }
-                    }
-                }
-            }
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 80.dp, start = 16.dp, end = 16.dp)
-                    .fillMaxWidth()
-                    .height(48.dp)
-                    .background(color = Color.White, shape = RoundedCornerShape(32.dp))
-            ) {
-                IconButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            horizontalPagerState.animateScrollToPage(page - 1)
-                        }
                     },
-                    modifier = Modifier
-                        .indication(MutableInteractionSource(), rememberRipple())
-                        .align(Alignment.CenterStart)
-                        .padding(vertical = 8.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_arrow_left),
-                        contentDescription = null,
+                    state = pagerState
+                ) { page2 ->
+                    val p2 = page2 % pages[p1].child.size
+                    HomeCard(
+                        text = pages[p1].child[p2],
+                        id = "$p1$p2".toInt() + random,
+                        modifier = Modifier
+                            .scale(scale.value)
+                            .graphicsLayer {
+                                selected.value = currentPageOffset != 0f
+                            }
                     )
                 }
-                IconButton(
-                    onClick = {
-                        coroutineScope.launch {
-                            horizontalPagerState.animateScrollToPage(page + 1)
-                        }
-                    },
-                    modifier = Modifier
-                        .indication(MutableInteractionSource(), rememberRipple())
-                        .align(Alignment.CenterEnd)
-                        .padding(vertical = 8.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_arrow_right),
-                        contentDescription = null,
-                    )
-                }
-                Text(
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .align(Alignment.Center),
-                    text = features[page % features.size].title.uppercase(),
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = TextUnit(16f, TextUnitType.Sp),
-                )
             }
+
+            HomeBottomButton(
+                modifier = Modifier.align(Alignment.BottomCenter),
+                horizontalPagerState,
+                pages
+            )
         }
     }
 }
 
-class Feature(val title: String, val list: List<String>)
-
+@ExperimentalCoilApi
 @Composable
-private fun DummyProgress(dialogState: Boolean, onDismiss: () -> Unit) {
-    if (dialogState) {
-        Dialog(
-            onDismissRequest = onDismiss,
-            properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .size(64.dp)
-                    .background(Color.White, shape = CircleShape)
-            ) {
-                CircularProgressIndicator()
-            }
-        }
-    }
-}
-
-@Composable
-fun InformationView() {
-    var dialogState by remember { mutableStateOf(false) }
-    DummyProgress(dialogState = dialogState) { dialogState = false }
-    Button(
-        onClick = {
-            dialogState = true
-            Handler(Looper.getMainLooper()).postDelayed(500) {
-                dialogState = false
-            }
-        }
+fun HomeCard(
+    text: String,
+    id: Int,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier,
     ) {
-        Text(text = "Hello world")
+            Image(
+                painter = rememberImagePainter(
+                    data = rememberRandomSampleImageUrl(
+                        id = id,
+                        width = 600,
+                        height = 1200
+                    ),
+                ),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(15.dp))
+            )
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            backgroundColor = Color.White,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .width(120.dp)
+                .height(120.dp)
+        ) {
+            Text(
+                text = text,
+                fontWeight = FontWeight.ExtraBold,
+                style = TextStyle(fontSize = 16.sp),
+                modifier = Modifier
+                    .wrapContentSize(),
+            )
+        }
     }
 }
 
-//@Composable
-//private fun ProfilePicture(seed: Int, modifier: Modifier = Modifier) {
-//    Card(
-//        modifier = modifier,
-//        shape = CircleShape,
-//        border = BorderStroke(4.dp, MaterialTheme.colors.surface)
-//    ) {
-//        Image(
-//            painter = rememberImagePainter(rememberRandomSampleImageUrl(seed)),
-//            contentDescription = null,
-//            modifier = Modifier.size(72.dp),
-//        )
-//    }
-//}
+@ExperimentalPagerApi
+@Composable
+fun HomeBottomButton(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    pages: List<PageData>
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Box(
+        modifier = modifier
+            .padding(start = 48.dp, end = 48.dp, bottom = 80.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .background(Color.White.copy(alpha = 0.95f))
+            .height(48.dp)
+            .fillMaxWidth()
+    ) {
+        IconButton(
+            onClick = {
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                }
+            },
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_left),
+                contentDescription = null,
+            )
+        }
 
-private val rangeForRandom = (0..100000)
+        Text(
+            text = pages[pagerState.currentPage % pages.size].title,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            style = TextStyle(
+                color = Color(0xFF4b4b4b),
+                fontSize = 16.sp
+            ),
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        IconButton(
+            onClick = {
+                coroutineScope.launch {
+                    pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                }
+            },
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_right),
+                contentDescription = null,
+            )
+        }
+    }
+}
 
 @Composable
 fun rememberRandomSampleImageUrl(
-    seed: Int,
-    width: Int = 200,
-    height: Int = 200,
-): String = remember { "https://picsum.photos/seed/$seed/$width/$height" }
+    id: Int,
+    width: Int,
+    height: Int,
+): String = remember { "https://picsum.photos/seed/$id/$width/$height" }
